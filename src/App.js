b886fc8ff1db6dd2a51da22823fc3ea08e0029b0
isambarboza <isabelabarboza17@gmail.com>
2023-08-29 16:39:21 -0300

    Todo funcionando

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+
+  const[listaTarefas, setListaTarefas] = useState([]);
+  const[tarefa,setTarefa] = useState( {id: '', texto: ""} );
+
+  function addTarefa()
+  {
+    if( tarefa.texto !==""){
+    setListaTarefas([...listaTarefas, tarefa]);
+    }
+  }
+  function excluirTarefa( id){
+    const novaLista = listaTarefas.filter((tarefa) => tarefa.id !==id);
+    setListaTarefas(novaLista);
+
+  }
+  useEffect(() => {
+    setTarefa( {id: "", texto:" "});
+  }, [listaTarefas])
+
   return (
     <>
+    <header>
+    <h1>Tarefas da semana 🌸</h1>
+    </header>
+    <div>
+      <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value })} />
+      <button onClick={addTarefa}>+</button>
+     
+    </div>
+    <div>
+      <ul>
+      {listaTarefas.map( (item, index) => (
+      <li key={index}>{item.texto} <button onClick={() => excluirTarefa(item.id)}>X</button></li>  
+      ))}
+      </ul>
+    </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,4 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+

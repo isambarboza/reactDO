98abd4c792da0acbdbaa169c9060895b71f9264e
isambarboza <isabelabarboza17@gmail.com>
2023-08-30 16:40:08 -0300

    projeto pronto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import "./App.css"
 function App() {
 
   const [ listaTarefas, setListaTarefas ] = useState ([]);
@@ -33,21 +33,25 @@ function App() {
 
   return (
     <>
-    <header>
-      <h1>TO DO LIST</h1>
-    </header>
-      <div>
-        <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value, status: false })} />
-        <button onClick={addTarefa}>Add itens</button>
     
-      </div>
+    <header>
+      <h1 className="tarefas">Tarefas da semana </h1>
+    </header>    
       <div>
+        <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value, status: false })} />  
         <ul> 
           {listaTarefas.map( (item, index ) => (
-           <li key={item.id}>{item.texto}<button  onClick={() => concluirTarefa(item.id, item.status) }>{item.status ? 'Concluida' : 'Não Concluida'}</button> <button onClick={() => excluirTarefa(item.id) }>Excluir</button></li>
+          <li key={item.id}> <div className="texto">{item.texto}</div><button className="statusTarefa" onClick={() => concluirTarefa(item.id, item.status) }> {item.status ? '🟢' : '🔴'}</button>  <button className="excluirTarefa" onClick={() => excluirTarefa(item.id) }>Excluir</button></li>
           ))}
         </ul>
       </div>
+      <div className="teste">
+        <button onClick={addTarefa} className="adicionar">✚</button>
+      </div>
+      <div>
+        <h1 className="rodape"> 📖</h1>
+      </div>
+       
     </>
    
   );
